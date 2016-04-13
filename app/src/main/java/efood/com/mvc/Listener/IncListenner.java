9ce462565da790b33b70acc3a59724b5529ba748
Lộc <anhyeuemlathat296@gmail.com>
2016-04-13 17:12:47 +0700

    Update IncListenner.java

--- app/src/main/java/efood/com/mvc/Listener/IncListenner.java
+++ app/src/main/java/efood/com/mvc/Listener/IncListenner.java
@@ -4,8 +4,7 @@ package efood.com.mvc.Listener;
  * Created by loc on 13/04/2016.
  */
 public interface IncListenner {
-    // lientienner là nơi thực hiện sữu lý dũ liệu
-    // khoong cos gia tri tra ve cái gi nên không cân truyên dữ liệu vào
+
     void Insert(String People);
 
 
